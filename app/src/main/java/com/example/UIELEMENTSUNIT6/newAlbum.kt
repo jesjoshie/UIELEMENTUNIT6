b2a1f503eef1fc160b477009cf11590925935e6a
Joshie <kalilinux19701375@gmail.com>
2020-11-26 17:27:39 +0800

    added newalbum

---
+++ app/src/main/java/com/example/UIELEMENTSUNIT6/newAlbum.kt
@@ -0,0 +1,11 @@
+package com.example.UIELEMENTSUNIT6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class newAlbum : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_album)
+    }
+}
